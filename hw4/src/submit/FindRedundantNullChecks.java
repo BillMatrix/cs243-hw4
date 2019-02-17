00904f6aa0c56d4529744a735bd942985517ed63
billmatrix <zheqzhu@stanford.edu>
2019-02-17 15:25:51 -0800

    direct import

--- hw4/src/submit/FindRedundantNullChecks.java
+++ hw4/src/submit/FindRedundantNullChecks.java
@@ -4,6 +4,8 @@ import joeq.Main.Helper;
 import joeq.Class.*;
 import joeq.Compiler.Quad.*;
 import flow.Flow.*;
+import submit.Hw4Solver;
+import submit.RedundantNull;
 
 public class FindRedundantNullChecks {
 
@@ -14,26 +16,26 @@ public class FindRedundantNullChecks {
      */
     public static void main(String[] args) {
         String solver_name = "submit.Hw4Solver";
-        Solver solver;
-        try {
-            Object solver_obj = Class.forName(solver_name).newInstance();
-            solver = (Solver) solver_obj;
-        } catch (Exception ex) {
-            System.out.println("ERROR: Could not load class '" + solver_name +
-                "' as Solver: " + ex.toString());
-            return;
-        }
+        Hw4Solver solver = new Solver();
+        // try {
+        //     Object solver_obj = Class.forName(solver_name).newInstance();
+        //     solver = (Solver) solver_obj;
+        // } catch (Exception ex) {
+        //     System.out.println("ERROR: Could not load class '" + solver_name +
+        //         "' as Solver: " + ex.toString());
+        //     return;
+        // }
 
         String analysis_name = "submit.RedundantNull";
-        Analysis analysis;
-        try {
-            Object analysis_obj = Class.forName(analysis_name).newInstance();
-            analysis = (Analysis) analysis_obj;
-        } catch (Exception ex) {
-            System.out.println("ERROR: Could not load class '" + analysis_name +
-                "' as Analysis: " + ex.toString());
-            return;
-        }
+        RedundantNull analysis = new Analysis();
+        // try {
+        //     Object analysis_obj = Class.forName(analysis_name).newInstance();
+        //     analysis = (Analysis) analysis_obj;
+        // } catch (Exception ex) {
+        //     System.out.println("ERROR: Could not load class '" + analysis_name +
+        //         "' as Analysis: " + ex.toString());
+        //     return;
+        // }
 
         jq_Class[] classes = new jq_Class[args.length];
         for (int i=0; i < classes.length; i++)
