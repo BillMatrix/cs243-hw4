94f668bc10ff89fdfc7b57d2c1fba1cea45fdfa1
billmatrix <zheqzhu@stanford.edu>
2019-02-17 15:27:00 -0800

    delete comment

--- hw4/src/submit/FindRedundantNullChecks.java
+++ hw4/src/submit/FindRedundantNullChecks.java
@@ -17,25 +17,9 @@ public class FindRedundantNullChecks {
     public static void main(String[] args) {
         String solver_name = "submit.Hw4Solver";
         Hw4Solver solver = new Hw4Solver();
-        // try {
-        //     Object solver_obj = Class.forName(solver_name).newInstance();
-        //     solver = (Solver) solver_obj;
-        // } catch (Exception ex) {
-        //     System.out.println("ERROR: Could not load class '" + solver_name +
-        //         "' as Solver: " + ex.toString());
-        //     return;
-        // }
 
         String analysis_name = "submit.RedundantNull";
         RedundantNull analysis = new RedundantNull();
-        // try {
-        //     Object analysis_obj = Class.forName(analysis_name).newInstance();
-        //     analysis = (Analysis) analysis_obj;
-        // } catch (Exception ex) {
-        //     System.out.println("ERROR: Could not load class '" + analysis_name +
-        //         "' as Analysis: " + ex.toString());
-        //     return;
-        // }
 
         jq_Class[] classes = new jq_Class[args.length];
         for (int i=0; i < classes.length; i++)
